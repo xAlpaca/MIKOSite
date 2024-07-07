61d2ff634665360d9f2e32338002950cf8bcd8ea
podhay123 <kuba.pod123@gmail.com>
2024-07-07 15:31:57 +0200

    footer

--- mikosite/mainSite/views.py
+++ mikosite/mainSite/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from mainSite.models import Post
 from django.http import HttpResponse
+
 # Create your views here.
 
+
 def index(request):
-    
+
     posts = Post.objects.all()
     reversed_posts = reversed(posts)
-    context = {'posts': reversed_posts}
-    
-    context['user'] = request.user
-    return render(request, 'index.html', context)
+    context = {"posts": reversed_posts}
 
+    context["user"] = request.user
+    return render(request, "index.html", context)
 
 
 def about(request):
-    
-    return render(request, 'about.html')
+
+    return render(request, "about.html")
